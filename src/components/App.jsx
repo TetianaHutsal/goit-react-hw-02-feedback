4bfa2467256cc0c89de7def7f8c239a0a42b5b7c
TetianaH67 <gutsalt9@gmail.com>
2023-09-10 19:56:37 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import FeedbackOptions from './FeedbackOptions';
 import Statistics from './Statistics';
 import styled from 'styled-components';
@@ -14,78 +14,60 @@ const Container = styled.div`
   color: #1856ff;
 `;
 
-const ButtonsContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  gap: 16px;
-  margin-top: 16px;
-
-  button {
-    width: 100px;
-    font-size: 18px;
-    padding: 8px 16px;
-    background-color: #029bff;
-    color: beige;
-    border: 10px;
-    border-radius: 15%;
-    cursor: pointer;
-
-    &:hover {
-      background-color: #029bff;
-    }
-  }
-`;
-
-const FeedbackContainer = styled.div`
+const Section = styled.div`
   margin-top: 24px;
   text-align: left;
   width: 300px;
   margin: 0 auto;
 `;
 
-const FeedbackItem = styled.p`
+const Notification = styled.p`
   margin: 4px 0;
   color: #0c2269;
 `;
 
-export const App = () => {
-  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+class App extends Component {
+  state = {
+    feedback: { good: 0, neutral: 0, bad: 0 },
+  };
 
-  const handleFeedback = type => {
-    setFeedback(prevFeedback => ({
-      ...prevFeedback,
-      [type]: prevFeedback[type] + 1,
+  handleFeedback = (type) => {
+    this.setState((prevState) => ({
+      feedback: { ...prevState.feedback, [type]: prevState.feedback[type] + 1 },
     }));
   };
 
-  const { good, neutral, bad } = feedback;
-  const total = good + neutral + bad;
-  const positivePercentage = total === 0 ? 0 : Math.round((good / total) * 100);
+  render() {
+    const { good, neutral, bad } = this.state.feedback;
+    const total = good + neutral + bad;
+    const positivePercentage = total === 0 ? 0 : Math.round((good / total) * 100);
 
-  return (
-    <Container>
-      <h2>Please leave your feedback</h2>
-      <ButtonsContainer>
-        <FeedbackOptions
-          options={['good', 'neutral', 'bad']}
-          onLeaveFeedback={handleFeedback}
-        />
-      </ButtonsContainer>
-
-      {total > 0 ? (
-        <FeedbackContainer>
+    return (
+      <Container>
+        <h2>Plese leave feedback</h2>
+        <Section>
           <h2>Statistics</h2>
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            positivePercentage={positivePercentage}
+          {total > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          ) : (
+            <Notification>There is no feedback</Notification>
+          )}
+        </Section>
+        <Section>
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.handleFeedback}
           />
-        </FeedbackContainer>
-      ) : (
-        <FeedbackItem>There is no feedback</FeedbackItem>
-      )}
-    </Container>
-  );
-};
+        </Section>
+      </Container>
+    );
+  }
+}
+
+export default App;
